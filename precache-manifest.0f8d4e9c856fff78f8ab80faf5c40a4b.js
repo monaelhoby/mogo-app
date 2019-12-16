856f56ea65ddca10b1e5df97788f19803592ee31
mona mohamed <monaelhoby@yahoo.com>
2019-12-17 00:03:19 +0200

    Updates

---
+++ precache-manifest.0f8d4e9c856fff78f8ab80faf5c40a4b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b7a9afd7b95f62e6ee8a72930bfb9ed",
+    "url": "/mogo-app/static/media/fa-brands-400.8b7a9afd.woff"
+  },
+  {
+    "revision": "ef632bfcd7f72a51f5ac",
+    "url": "/mogo-app/static/css/main.747982de.chunk.css"
+  },
+  {
+    "revision": "4478b4d7022cad174e4c04246fe622ef",
+    "url": "/mogo-app/static/media/fa-solid-900.4478b4d7.svg"
+  },
+  {
+    "revision": "35d3fb6bb05a668b0172",
+    "url": "/mogo-app/static/js/1.35d3fb6b.chunk.js"
+  },
+  {
+    "revision": "91951b0073c744fb5392",
+    "url": "/mogo-app/static/js/runtime~main.91951b00.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/mogo-app/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "bdadb6ce95c5a2e7b673940721450d3c",
+    "url": "/mogo-app/static/media/fa-regular-400.bdadb6ce.woff2"
+  },
+  {
+    "revision": "0b5e3a5451fc62d9023ccafc85bc89db",
+    "url": "/mogo-app/static/media/fa-regular-400.0b5e3a54.woff"
+  },
+  {
+    "revision": "b48c48ea8457846a5695b139c377d3d1",
+    "url": "/mogo-app/static/media/fa-regular-400.b48c48ea.ttf"
+  },
+  {
+    "revision": "6493321d567eb0f22bd5112fbcf044a8",
+    "url": "/mogo-app/static/media/fa-regular-400.6493321d.eot"
+  },
+  {
+    "revision": "659c4d58b00226541ef95c3a76e169c5",
+    "url": "/mogo-app/static/media/fa-brands-400.659c4d58.woff2"
+  },
+  {
+    "revision": "ef632bfcd7f72a51f5ac",
+    "url": "/mogo-app/static/js/main.ef632bfc.chunk.js"
+  },
+  {
+    "revision": "fb493903265cad425ccdf8e04fc2de61",
+    "url": "/mogo-app/static/media/fa-solid-900.fb493903.woff2"
+  },
+  {
+    "revision": "bcb927a742a8370b76642fd1a9a749c0",
+    "url": "/mogo-app/static/media/fa-solid-900.bcb927a7.woff"
+  },
+  {
+    "revision": "ec0716ae8aa1ba781a1a6bcbce833f6c",
+    "url": "/mogo-app/static/media/fa-brands-400.ec0716ae.eot"
+  },
+  {
+    "revision": "b69de69a4ff8ca0abe96ec0b0c180c5b",
+    "url": "/mogo-app/static/media/fa-brands-400.b69de69a.ttf"
+  },
+  {
+    "revision": "0c41971339b9fc5b1cefb0abad1e2e69",
+    "url": "/mogo-app/static/media/fa-regular-400.0c419713.svg"
+  },
+  {
+    "revision": "f29ad0031ad2c1c14b771ce504e2bfa7",
+    "url": "/mogo-app/static/media/fa-solid-900.f29ad003.eot"
+  },
+  {
+    "revision": "48f54f63d7711d0912a9a10205538fc4",
+    "url": "/mogo-app/static/media/fa-solid-900.48f54f63.ttf"
+  },
+  {
+    "revision": "42f9fd6acee87559ac0d6a33488db65e",
+    "url": "/mogo-app/static/media/fa-brands-400.42f9fd6a.svg"
+  },
+  {
+    "revision": "35d3fb6bb05a668b0172",
+    "url": "/mogo-app/static/css/1.add462ca.chunk.css"
+  },
+  {
+    "revision": "4f12848bb4c5357cd7ec743485d4c871",
+    "url": "/mogo-app/index.html"
+  }
+];
